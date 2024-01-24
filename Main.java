9b63c60b8b5e8d64b160956f7860216cdfffc5c6
yozora <128562373+louwisalfred@users.noreply.github.com>
2024-01-24 20:54:02 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package VariableScope;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DiceRoller DR = new DiceRoller();
+		DiceRoller DR2 = new DiceRoller();
+		DiceRoller DR3 = new DiceRoller();
+	}
+
+}
